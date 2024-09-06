b38a5cfa0de49ecbd74de9d2059da956ead3e989
Caio Vinicius <caiovpsilveira@gmail.com>
2024-09-06 20:27:08 -0300

    Moved to WorkClient

---
+++ src/loadbalancer/server/WorkClient.java
@@ -0,0 +1,65 @@
+package loadbalancer.server;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class WorkClient {
+    private SocketChannel socketChannel;
+
+    public WorkClient(String addr, int port) {
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+
+            // Initiate connection to the server
+            InetSocketAddress server = new InetSocketAddress(addr, port);
+            socketChannel.connect(server);
+
+            // Wait until the connection is completed
+            while (!socketChannel.finishConnect()) {
+                System.out.println("Connecting to server...");
+            }
+            System.out.println("Connected to server!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void finalize() {
+        try {
+            socketChannel.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            try {
+                int bytesRead = socketChannel.read(buffer);
+
+                // If bytesRead == -1, the server has closed the connection
+                if (bytesRead == -1) {
+                    System.out.println("Server has closed the connection.");
+                    break;
+                }
+
+                // If there's data to read, process it
+                if (bytesRead > 0) {
+                    buffer.flip(); // Switch buffer from write to read mode
+                    byte[] data = new byte[bytesRead];
+                    buffer.get(data); // Retrieve data from buffer
+
+                    String receivedMessage = new String(data);
+                    System.out.println("Received from server: " + receivedMessage);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
