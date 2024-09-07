b7d2a0c1831b7b99444479bdf9fc7667394d6aff
Caio Vinicius <caiovpsilveira@gmail.com>
2024-09-06 21:03:25 -0300

    Added Payload class

---
+++ src/loadbalancer/Payload.java
@@ -0,0 +1,56 @@
+package loadbalancer;
+
+import java.nio.ByteBuffer;
+
+public class Payload {
+    private byte[] ip; // the first byte is the left-most IP value, IPv4
+    private int time;
+
+    Payload(ByteBuffer buf) {
+        ip = new byte[4];
+        buf.flip();
+        buf.get(ip, 0, 4);
+        time = buf.getInt();
+    }
+
+    Payload(String ipStr, int time)
+    {
+        String[] values = ipStr.split("\\.");
+        ip = new byte[4];
+
+        for (int i = 0; i < values.length; ++i) {
+            ip[i] = (byte) Integer.parseInt(values[i]);
+        }
+        this.time = time;
+    }
+
+    String getIP() {
+        return Byte.toString(ip[0]) + "." +  Byte.toString(ip[1]) + "." +  Byte.toString(ip[2]) + "." +  Byte.toString(ip[3]);
+    }
+
+    int getTime() {
+        return time;
+    }
+
+    // returns a non-flipped ByteBuffer with capacity 8, and position at the end
+    ByteBuffer toByteBuffer() {
+        ByteBuffer buf = ByteBuffer.allocate(8);
+        buf.put(ip);
+        buf.putInt(time);
+        return buf;
+    }
+
+    public static void main(String[] args) {
+        Payload p1 = new Payload("192.168.1.2", 35);
+
+        assert(p1.getIP().equals("192.168.1.2"));
+        assert(p1.getTime() == 35);
+
+        ByteBuffer buf = p1.toByteBuffer();
+        assert(buf.remaining() == 8);
+        Payload p2 = new Payload(buf);
+
+        assert(p2.getIP().equals("192.168.1.2"));
+        assert(p2.getTime() == 35);
+    }
+}
